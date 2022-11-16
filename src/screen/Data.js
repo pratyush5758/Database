31fd2f45cb49cb779aa28b56a589d67412982825
Pratyush Kumar <pratyushkumar768@gmail.com>
2022-11-16 20:38:14 +0530

    change in data

--- src/screen/Data.js
+++ src/screen/Data.js
@@ -6,40 +6,44 @@ const Data = () => {
   const route = useRoute();
   console.log(route);
   return (
-     <View style={{paddingHorizontal:10}}>
-      <View style={{padding:20,shadowColor: "#000",
-      marginTop:10,
-      backgroundColor:'#fff',
-      borderRadius:10,
-shadowOffset: {
-	width: 0,
-	height: 2,
-},
-shadowOpacity: 0.25,
-shadowRadius: 4.84,
+    <View style={{paddingHorizontal: 10}}>
+      <View
+        style={{
+          padding: 20,
+          shadowColor: '#000',
+          marginTop: 10,
+          backgroundColor: '#fff',
+          borderRadius: 10,
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 4.84,
 
-elevation: 5,}}>
-      <View style={styles.FirstNameContainer}>
-        <View>
-          <Text style={styles.NameText}>First Name</Text>
-          <Text style={styles.text}>{route.params.firstname}</Text>
+          elevation: 5,
+        }}>
+        <View style={styles.FirstNameContainer}>
+          <View>
+            <Text style={styles.NameText}>First Name</Text>
+            <Text style={styles.text}>{route.params.firstname}</Text>
+          </View>
+          <View>
+            <Text style={styles.NameText}>Last Name</Text>
+            <Text style={styles.text}>{route.params.lastname}</Text>
+          </View>
         </View>
-       <View>
-        <Text style={styles.NameText}>Last Name</Text>
-        <Text style={styles.text}>{route.params.lastname}</Text>
-       </View>
+        <Text style={styles.text}>{route.params.email}</Text>
+        <Text style={styles.text}>{route.params.dateofbirth}</Text>
+        <Text style={styles.text}>{route.params.phonenumber}</Text>
+        <Text style={styles.text}>{route.params.state}</Text>
+        <Text style={styles.text}>{route.params.city}</Text>
+        <Text style={styles.text}>{route.params.pincode}</Text>
+        <Text style={styles.text}>{route.params.plotno}</Text>
+        <Text style={styles.text}>{route.params.address}</Text>
+        <Text style={styles.text}>{route.params.countryname}</Text>
       </View>
-      <Text style={styles.text}>{route.params.email}</Text>
-      <Text style={styles.text}>{route.params.dateofbirth}</Text>
-      <Text style={styles.text}>{route.params.phonenumber}</Text>
-      <Text style={styles.text}>{route.params.state}</Text>
-      <Text style={styles.text}>{route.params.city}</Text>
-      <Text style={styles.text}>{route.params.pincode}</Text>
-      <Text style={styles.text}>{route.params.plotno}</Text>
-      <Text style={styles.text}>{route.params.address}</Text>
-      <Text style={styles.text}>{route.params.countryname}</Text>
     </View>
-     </View>
   );
 };
 
@@ -47,7 +51,7 @@ export default Data;
 const styles = StyleSheet.create({
   text: {
     // borderBottomWidth:1,
-    marginTop:4,
+    marginTop: 4,
     fontWeight: 'bold',
   },
   NameText: {
@@ -55,8 +59,8 @@ const styles = StyleSheet.create({
     color: 'blue',
     fontSize: 17,
   },
-  FirstNameContainer:{
-    flexDirection:'row',
-    justifyContent:'space-between'
-  }
+  FirstNameContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
 });
